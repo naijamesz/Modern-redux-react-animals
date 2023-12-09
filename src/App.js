45a96c6d14e30f4250d1b84c4a7347b66c28268b
naijamesz <kodlikejamesz@hotmail.com>
2023-12-09 08:19:54 +0700

    useState example.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [count, setCount] = useState(0);
   const handleClick = () => {
+    setCount(count + 1);
     console.log('Button was clicked!');
   };
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      {/* <button onClick={handleClick()}>Bad Event Handler</button> */}
+      <button onClick={() => setCount(count ? count - 1 : count >= 0)}>Remove Animal</button>
+      <button onClick={() => setCount(count * 0)}>Reset</button>
+      <div>Number of animals: {count}</div>
     </div>
   );
 }
